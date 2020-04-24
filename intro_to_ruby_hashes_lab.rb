e1cacba077fb00e2f06841bfaee5b4297098d3dc
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-04-24 19:50:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,9 +32,10 @@ end
 def monopoly_with_third_tier
    new_hash = {
    :railroads => {
-     :pieces => "4",
+     :pieces => 4,
      :rent_in_dollars => {
-       :one_piece_owned => 
+       :one_piece_owned => 25
+       
      },
      :names => {}
    }
